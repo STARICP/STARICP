cced0e827ac31672df43908c97f19bc0e020d3bc
MengCheng1015 <858753441@qq.com>
2020-12-13 01:48:02 +0800

    bug

--- JS/stop.js
+++ JS/stop.js
@@ -1,6 +1,6 @@
 Swal.fire({
   type: 'error',
-  title: '��ͣ��ѯ����',
-  text: '���������˼������⣡�����޸���',
-  footer: '<a href="https://github.com/STARICP/STARICP/">Ҫ���������ǵ�github�ֿ���Сstar��</a>'
+  title: '本站查询功能暂停服务！',
+  text: '我们遇到了一些技术问题，正在努力修复！',
+  footer: '<a href="https://github.com/STARICP/STARICP/">要不先来我们的github仓库点个小star？</a>'
 })
