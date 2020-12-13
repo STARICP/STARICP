705c3d76b06df04fbad556315ceece13b6f88fe7
YellowFace <68597070+yellowface233@users.noreply.github.com>
2020-12-13 11:40:32 +0800

    Create search.js

---
+++ JS/search.js
@@ -0,0 +1,4 @@
+//Powered by StarICP(icp.bili.bio).
+//星ICP备查询系统核心
+
+;var ru="//auth.icp.bili.bio";function s(){console.log("search request received");var keyword=document.getElementById('keyword').value;window.location.href=ru+"/"+keyword;}
