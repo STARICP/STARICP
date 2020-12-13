1d6873cfd52cc106e7cbce4c27b01507fed35bf8
YellowFace <68597070+yellowface233@users.noreply.github.com>
2020-12-13 11:59:46 +0800

    search javascript

--- JS/search.js
+++ JS/search.js
@@ -1,4 +1,1 @@
-//Powered by StarICP(icp.bili.bio).
-//星ICP备查询系统核心
-
 ;var ru="//auth.icp.bili.bio";function s(){console.log("search request received");var keyword=document.getElementById('keyword').value;window.location.href=ru+"/"+keyword;}
