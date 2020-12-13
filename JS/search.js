234696ced232e24f319a0a5c3b0acd09c30bb6aa
MengCheng1015 <858753441@qq.com>
2020-12-13 12:55:55 +0800

    update

--- JS/search.js
+++ JS/search.js
@@ -1,1 +1,1 @@
-;var ru="//auth.icp.bili.bio";function s(){console.log("search request received");var keyword=document.getElementById('keyword').value;window.location.href=ru+"/"+keyword;}
+;var ru="//search.icp.bili.bio";function s(){console.log("search request received");var keyword=document.getElementById('keyword').value;window.location.href=ru+"/"+keyword;}
